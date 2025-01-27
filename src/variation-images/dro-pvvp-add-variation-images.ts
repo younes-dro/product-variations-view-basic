b6bc4c4621f96def79944ecf0ec8617989670afa
Younes DRO <31171+younes-dro@users.noreply.github.com>
2025-01-27 17:10:57 +0100

    Implement Singleton pattern and add trigger for WooCommerce variation load to display image buttons.
    - Introduced a Singleton pattern for the `ProductVariationImage` class to ensure a single instance.
    - Added logic to initialize and trigger the placement of image management buttons directly under the first options for each WooCommerce product variation.
    - Checked `wp.media` availability for proper functionality.

--- src/variation-images/dro-pvvp-add-variation-images.ts
+++ src/variation-images/dro-pvvp-add-variation-images.ts
@@ -1,19 +1,53 @@
 declare var wp: any;
 
 class ProductVariationImage {
-  static checkMediaAvailability(): void {
+  private static instance: ProductVariationImage | null = null;
+
+  private constructor() {}
+
+  public static getInstance(): ProductVariationImage {
+    if (this.instance == null) {
+      this.instance = new ProductVariationImage();
+    }
+    return this.instance;
+  }
+
+  public checkMediaAvailability(): boolean {
     if (typeof wp !== 'undefined' && typeof wp.media !== 'undefined') {
-      console.log('wp.media is loaded and available');
+      return true;
     } else {
-      console.error('wp.media is NOT available! Ensure wp_enqueue_media() is called.');
+      return false;
     }
   }
 
-  static test(): void {
-    ProductVariationImage.checkMediaAvailability();
-    console.log('Add Multiple Variation Images');
+  // initialize events
+  public init(): void {
+
+    jQuery('.woocommerce_variation').each((index, element) => {
+      var optionsWrapper = jQuery(element).find('.options:first');
+      var galleryWrapper = jQuery(element).find('.dro-pvvp-variation-images-container');
+      galleryWrapper.insertBefore(optionsWrapper);
+      
+    });
+    jQuery(document.body).on('click', '.dro-pvvp-handlediv', function () {
+      jQuery(this).closest('.dro-pvvp-variation-images-postbox').toggleClass('closed');
+      var ariaExpandedValue = !jQuery(this).closest('.dro-pvvp-variation-images-postbox').hasClass('closed');
+      jQuery(this).attr('aria-expanded', ''+ariaExpandedValue+'');
+    });
   }
 }
+(function ($) {
+  $(function () {
+    const variationImageManager = ProductVariationImage.getInstance();
+    if( variationImageManager.checkMediaAvailability()){
+      // variationImageManager.init();
+    $('#woocommerce-product-data').on('woocommerce_variations_loaded', function () {
+      variationImageManager.init();
+    });
 
-// Usage example
-ProductVariationImage.test();
+  }else{
+    console.error('wp.media is NOT available! Ensure wp_enqueue_media() is called.');
+  }
+    
+  });
+})(jQuery);
