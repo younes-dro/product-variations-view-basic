33066eae9b5d118d6531275b4004a83a10c6c9e7
Younes DRO <31171+younes-dro@users.noreply.github.com>
2025-07-16 20:59:11 +0100

    Refactor gallery factory unit tests for new layout API
    Updated the unit tests for DRO_PVVP_Gallery_Factory to use the new create_gallery_layout method and updated namespace imports.
    Removed tests for deprecated methods and added tests for layout instantiation and error handling with unknown layouts.

--- tests/unit/test-class-dro-pvvp-gallery-factory.php
+++ tests/unit/test-class-dro-pvvp-gallery-factory.php
@@ -10,13 +10,15 @@
 
 declare(strict_types=1);
 
-use DRO\PVVP\Includes\Factories\DRO_PVVP_Gallery_Factory;
-use DRO\PVVP\Includes\Interfaces\DRO_PVVP_Variation_Data_Provider_Interface;
-use WC_Product;
+use DRO\PVVP\Includes\Gallery\Factories\DRO_PVVP_Gallery_Factory as Gallery_Factory;
+use DRO\PVVP\Includes\Gallery\Layouts\DRO_PVVP_Default_Layout as Default_Layout;
+use DRO\PVVP\Includes\Gallery\Interfaces\DRO_PVVP_Gallery_Interface as Gallery_Interface;
 
 
 /**
  * Unit tests for the DRO_PVVP_Gallery_Factory class.
+ *
+ * @coversDefaultClass \DRO\PVVP\Includes\Factories\DRO_PVVP_Gallery_Factory
  */
 class DRO_PVVP_Gallery_Factory_Test extends WP_UnitTestCase {
 
@@ -26,7 +28,7 @@ class DRO_PVVP_Gallery_Factory_Test extends WP_UnitTestCase {
 	 *
 	 * @var DRO_PVVP_Gallery_Factory
 	 */
-	private ?DRO_PVVP_Gallery_Factory $gallery_factory;
+	private ?Gallery_Factory $gallery_factory;
 
 	/**
 	 * Set up the test environment.
@@ -35,7 +37,7 @@ class DRO_PVVP_Gallery_Factory_Test extends WP_UnitTestCase {
 	 */
 	public function setUp(): void {
 		parent::setUp();
-		$this->gallery_factory = DRO_PVVP_Gallery_Factory::get_instance();
+		$this->gallery_factory = Gallery_Factory::get_instance();
 	}
 
 	/**
@@ -51,103 +53,43 @@ class DRO_PVVP_Gallery_Factory_Test extends WP_UnitTestCase {
 	/**
 	 * Test the get_instance method of the Gallery Factory.
 	 *
+	 * @covers ::get_instance
 	 * @return void
 	 */
 	public function test_get_instance(): void {
-		$gallery_factory_1 = DRO_PVVP_Gallery_Factory::get_instance();
-		$gallery_factory_2 = DRO_PVVP_Gallery_Factory::get_instance();
+		$gallery_factory_1 = Gallery_Factory::get_instance();
+		$gallery_factory_2 = Gallery_Factory::get_instance();
 
-		$this->assertInstanceOf( DRO_PVVP_Gallery_Factory::class, $this->gallery_factory, 'Gallery Factory instance is not of the correct class.' );
+		$this->assertInstanceOf( Gallery_Factory::class, $this->gallery_factory, 'Gallery Factory instance is not of the correct class.' );
 		$this->assertSame( $gallery_factory_1, $this->gallery_factory, 'Gallery Factory instance is not the same as the one returned by get_instance() method.' );
 		$this->assertSame( $gallery_factory_1, $gallery_factory_2, 'Multiple calls to get_instance() should return the same instance.' );
 		$this->assertSame( $gallery_factory_1, $gallery_factory_2 );
 	}
 
 	/**
-	 * Test the create_gallery_for_product method.
+	 * Test the create_gallery_layout method of the Gallery Factory.
 	 *
-	 * @covers \DRO\PVVP\Includes\Factories\DRO_PVVP_Gallery_Factory::create_gallery_for_product
+	 * @covers ::create_gallery_layout
+	 * @return void
 	 */
-	public function test_create_gallery_for_product() {
-		// Mock the data provider
-		$data_provider_mock = $this->createMock( DRO_PVVP_Variation_Data_Provider_Interface::class );
+	public function test_create_gallery_layout(): void {
+		$layout_config = array( 'layout' => 'default' );
 
-		// Mock the product
-		$product_mock = $this->createMock( WC_Product::class );
-		$product_mock->method( 'is_type' )->with( 'variable' )->willReturn( true );
+		$presumed_default_layout = $this->gallery_factory->create_gallery_layout( $layout_config );
 
-		
-		// Case 1: Product is not a variable product (expect exception)
-		$product_mock_not_variable = $this->createMock( WC_Product::class );
-		$product_mock_not_variable->method( 'is_type' )->with( 'variable' )->willReturn( false );
-
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-
-		$this->expectException( \InvalidArgumentException::class );
-		$this->expectExceptionMessage( 'Product must be a variable product.' );
-
-		$factory->create_gallery_for_product( $product_mock_not_variable );
-
-		// Case 2: No available variations
-		$data_provider_mock->method( 'get_available_variations' )->willReturn( array() );
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-		$this->assertNull( $factory->create_gallery_for_product( $product_mock ) );
-
-		// Case 3: Variations with no images
-		$data_provider_mock->method( 'get_available_variations' )->willReturn( array( array( 'variation_id' => 1 ) ) );
-		$data_provider_mock->method( 'get_variation_main_image' )->with( 1 )->willReturn( null );
-		$data_provider_mock->method( 'get_variation_thumbs' )->with( 1 )->willReturn( array() );
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-		$this->assertNull( $factory->create_gallery_for_product( $product_mock ) );
-
-		// Case 4: Variations with images
-		$data_provider_mock->method( 'get_available_variations' )->willReturn( array( array( 'variation_id' => 1 ) ) );
-		$data_provider_mock->method( 'get_variation_main_image' )->with( 1 )->willReturn( 'main_image.jpg' );
-		$data_provider_mock->method( 'get_variation_thumbs' )->with( 1 )->willReturn( array( 'thumb1.jpg' ) );
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-		$this->assertIsString( $factory->create_gallery_for_product( $product_mock ) );
+		$this->assertInstanceOf( Default_Layout::class, $presumed_default_layout );
+		$this->assertInstanceOf( Gallery_Interface::class, $presumed_default_layout );
 	}
-
 	/**
-	 * Test the create_gallery_for_variation method.
+	 * Test the create_gallery_layout method with an unknown layout.
 	 *
-	 * @covers \DRO\PVVP\Includes\Factories\DRO_PVVP_Gallery_Factory::create_gallery_for_variation
+	 * @covers ::create_gallery_layout
+	 * @return void
 	 */
-	public function test_create_gallery_for_variation() {
-		// Mock the data provider
-		$data_provider_mock = $this->createMock( DRO_PVVP_Variation_Data_Provider_Interface::class );
+	public function test_create_gallery_layout_with_unknown_layout(): void {
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Unknown gallery layout: unknown' );
 
-		// Mock the product and variation
-		$variation_mock = $this->createMock( WC_Product_Variation::class );
-		$variation_mock->method( 'get_parent_id' )->willReturn( 1 );
-
-		$product_mock = $this->createMock( WC_Product::class );
-
-		// Mock wc_get_product
-		if ( ! function_exists( 'wc_get_product' ) ) {
-			function wc_get_product( $id ) {
-				if ( $id === 1 ) {
-					return $GLOBALS['product_mock'];
-				}
-				if ( $id === 123 ) {
-					return $GLOBALS['variation_mock'];
-				}
-				return null;
-			}
-		}
-		$GLOBALS['product_mock']   = $product_mock;
-		$GLOBALS['variation_mock'] = $variation_mock;
-
-		// Case 1: Invalid variation
-		$data_provider_mock->method( 'is_valid_variation' )->with( 123 )->willReturn( false );
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-		$this->assertNull( $factory->create_gallery_for_variation( 123 ) );
-
-		// Case 2: Valid variation
-		$data_provider_mock->method( 'is_valid_variation' )->with( 123 )->willReturn( true );
-		$data_provider_mock->method( 'get_variation_main_image' )->with( 123 )->willReturn( 'main_image.jpg' );
-		$data_provider_mock->method( 'get_variation_thumbs' )->with( 123 )->willReturn( array( 'thumb1.jpg' ) );
-		$factory = new DRO_PVVP_Gallery_Factory( $data_provider_mock );
-		$this->assertIsString( $factory->create_gallery_for_variation( 123 ) );
+		$this->gallery_factory->create_gallery_layout( array( 'layout' => 'unknown' ) );
 	}
 }
